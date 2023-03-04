ae709aad17719ab627c87dc93df5d4e23eda676c
dezhiShen <1179551960@qq.com>
2023-03-05 00:26:33 +0800

    feat:增加插件生成说明

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ onebot-plus-plugin 模板工程
 3. 运行`go mod tidy`更新依赖
 4. 修改main.go中插件的逻辑
 ## 构建插件
+`sh build.sh $plugin_name`
+- 不同平台的插件将会生成在plugins目录下
--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 
 #export CGO_ENABLED=1
+plugin_name=$1
 mkdir -p ./plugins
+if [ -z "$plugin_name" ]; then
+    read -p "Enter the plugin name: " plugin_name
+    if [ -z "$plugin_name" ]; then
+        echo "Plugin name is empty"
+        exit 1
+    fi
+fi
 export GOARCH=amd64
 export GOOS=windows
-go build -o ./plugins/plugin-windows-amd64.exe ./main.go
+go build -o ./plugins/${plugin_name}-windows-amd64.exe ./main.go
 
 export GOARM=7
 export GOARCH=arm64
 export GOOS=linux
 
-go build -o ./plugins/plugin-linux-arm64v7 ./main.go
+go build -o ./plugins/${plugin_name}-linux-arm64v7 ./main.go
 
 export GOARCH=amd64
 export GOOS=darwin
-go build -o ./plugins/plugin-linux-amd64 ./main.go
+go build -o ./plugins/${plugin_name}-linux-amd64 ./main.go
 
